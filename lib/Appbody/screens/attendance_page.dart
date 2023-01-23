ed41ee0bf0aa82dd82a4eab03020388374568f83
LUNAGOK <21lunagok@gmail.com>
2023-01-23 21:01:48 +0530

    005

--- lib/Appbody/screens/attendance_page.dart
+++ lib/Appbody/screens/attendance_page.dart
@@ -133,44 +133,55 @@ class _AttendancePageState extends State<AttendancePage> {
                   ),
                   child: Column(
                     children: [
-                      SizedBox(
-                        height: 50,
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(7.0),
+                            child: Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Container(
+                                width: 190,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                  color: textColor,
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                child: TextField(
+                                  style: GoogleFonts.averiaGruesaLibre(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: maintextColor),
+                                  decoration: const InputDecoration(
+                                    hintText: "   enter Subject",
+                                    hintStyle: TextStyle(
+                                        fontSize: 15, color: Colors.white54),
+                                    border: InputBorder.none,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: Container(
+                              height: 40,
+                              width: 90,
+                              decoration: BoxDecoration(
+                                  color: lightblue,
+                                  borderRadius: BorderRadius.circular(30)),
+                              child: Center(
+                                child: Text(
+                                  "75 %",
+                                  style: GoogleFonts.averiaGruesaLibre(
+                                    color: textColor,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                      // Row(
-                      //   children: [
-                      //     Padding(
-                      //       padding: const EdgeInsets.fromLTRB(15, 0, 8, 0),
-                      //       child: Container(
-                      //         color: textColor,
-                      //         child: TextField(
-                      //           style: GoogleFonts.averiaGruesaLibre(
-                      //               color: maintextColor, fontSize: 25),
-                      //           decoration: const InputDecoration(
-                      //               border: InputBorder.none),
-                      //         ),
-                      //       ),
-                      //     ),
-                      //     Padding(
-                      //       padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
-                      //       child: Container(
-                      //         height: 40,
-                      //         width: 90,
-                      //         decoration: BoxDecoration(
-                      //             color: lightblue,
-                      //             borderRadius: BorderRadius.circular(30)),
-                      //         child: Center(
-                      //           child: Text(
-                      //             "75 %",
-                      //             style: GoogleFonts.averiaGruesaLibre(
-                      //               color: textColor,
-                      //               fontWeight: FontWeight.bold,
-                      //             ),
-                      //           ),
-                      //         ),
-                      //       ),
-                      //     ),
-                      //   ],
-                      // ),
                       const Divider(
                         color: mainbackground,
                         thickness: 2,
@@ -219,8 +230,43 @@ class _AttendancePageState extends State<AttendancePage> {
                             ),
                           ),
                           const SizedBox(
-                            width: 50,
+                            width: 40,
                           ),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                color: lightblue,
+                                shape: BoxShape.circle,
+                                border: Border.all(color: textColor),
+                              ),
+                              child: const Icon(
+                                Icons.add,
+                                color: textColor,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                color: lightblue,
+                                shape: BoxShape.circle,
+                                border: Border.all(color: textColor),
+                              ),
+                              child: const Icon(
+                                Icons.remove,
+                                color: textColor,
+                              ),
+                            ),
+                          )
                         ],
                       )
                     ],
@@ -229,197 +275,6 @@ class _AttendancePageState extends State<AttendancePage> {
                 const SizedBox(
                   height: 20,
                 ),
-                //
-                //2nd Container
-                //
-
-                // Container(
-                //   height: 100,
-                //   width: 320,
-                //   //color: textColor,
-                //   decoration: BoxDecoration(
-                //     color: textColor,
-                //     borderRadius: BorderRadius.circular(30),
-                //     boxShadow: [
-                //       BoxShadow(
-                //         color: Colors.grey.withOpacity(0.5),
-                //         spreadRadius: 3,
-                //         blurRadius: 10,
-                //         offset: const Offset(0, 3),
-                //       )
-                //     ],
-                //   ),
-                // ),
-                // const SizedBox(
-                //   height: 20,
-                // ),
-                // //
-                // //2nd Container
-                // //
-
-                // Container(
-                //   height: 100,
-                //   width: 320,
-                //   //color: textColor,
-                //   decoration: BoxDecoration(
-                //     color: textColor,
-                //     borderRadius: BorderRadius.circular(30),
-                //     boxShadow: [
-                //       BoxShadow(
-                //         color: Colors.grey.withOpacity(0.5),
-                //         spreadRadius: 3,
-                //         blurRadius: 10,
-                //         offset: const Offset(0, 3),
-                //       )
-                //     ],
-                //   ),
-                // ),
-                // const SizedBox(
-                //   height: 20,
-                // ),
-                // //
-                // //3nd Container
-                // //
-
-                // Container(
-                //   height: 100,
-
-                //   width: 320,
-                //   //color: textColor,
-                //   decoration: BoxDecoration(
-                //     color: textColor,
-                //     borderRadius: BorderRadius.circular(30),
-                //     boxShadow: [
-                //       BoxShadow(
-                //         color: Colors.grey.withOpacity(0.5),
-                //         spreadRadius: 3,
-                //         blurRadius: 10,
-                //         offset: const Offset(0, 3),
-                //       )
-                //     ],
-                //   ),
-                // ),
-                // const SizedBox(
-                //   height: 20,
-                // ),
-                // //
-                // //2nd Container
-                // //
-
-                // Container(
-                //   height: 100,
-                //   width: 320,
-                //   //color: textColor,
-                //   decoration: BoxDecoration(
-                //     color: textColor,
-                //     borderRadius: BorderRadius.circular(30),
-                //     boxShadow: [
-                //       BoxShadow(
-                //         color: Colors.grey.withOpacity(0.5),
-                //         spreadRadius: 3,
-                //         blurRadius: 10,
-                //         offset: const Offset(0, 3),
-                //       )
-                //     ],
-                //   ),
-                // ),
-                // const SizedBox(
-                //   height: 20,
-                // ),
-                // //
-                // //2nd Container
-                // //
-
-                // Container(
-                //   height: 100,
-                //   width: 320,
-                //   decoration: BoxDecoration(
-                //     color: textColor,
-                //     borderRadius: BorderRadius.circular(30),
-                //     boxShadow: [
-                //       BoxShadow(
-                //         color: Colors.grey.withOpacity(0.5),
-                //         spreadRadius: 3,
-                //         blurRadius: 10,
-                //         offset: const Offset(0, 3),
-                //       )
-                //     ],
-                //   ),
-                // ),
-                // const SizedBox(
-                //   height: 20,
-                // ),
-                // //
-                // //2nd Container
-                // //
-
-                // Container(
-                //   height: 100,
-                //   width: 320,
-                //   //color: textColor,
-                //   decoration: BoxDecoration(
-                //     color: textColor,
-                //     borderRadius: BorderRadius.circular(30),
-                //     boxShadow: [
-                //       BoxShadow(
-                //         color: Colors.grey.withOpacity(0.5),
-                //         spreadRadius: 3,
-                //         blurRadius: 10,
-                //         offset: const Offset(0, 3),
-                //       )
-                //     ],
-                //   ),
-                // ),
-                // const SizedBox(
-                //   height: 20,
-                // ),
-                // //
-                // //2nd Container
-                // //
-
-                // Container(
-                //   height: 100,
-                //   width: 320,
-                //   //color: textColor,
-                //   decoration: BoxDecoration(
-                //     color: textColor,
-                //     borderRadius: BorderRadius.circular(30),
-                //     boxShadow: [
-                //       BoxShadow(
-                //         color: Colors.grey.withOpacity(0.5),
-                //         spreadRadius: 3,
-                //         blurRadius: 10,
-                //         offset: const Offset(0, 3),
-                //       )
-                //     ],
-                //   ),
-                // ),
-                // const SizedBox(
-                //   height: 20,
-                // ),
-                // //
-                // //2nd Container
-                // //
-
-                // Container(
-                //   height: 100,
-                //   width: 320,
-                //   decoration: BoxDecoration(
-                //     color: textColor,
-                //     borderRadius: BorderRadius.circular(30),
-                //     boxShadow: [
-                //       BoxShadow(
-                //         color: Colors.grey.withOpacity(0.5),
-                //         spreadRadius: 3,
-                //         blurRadius: 10,
-                //         offset: const Offset(0, 3),
-                //       )
-                //     ],
-                //   ),
-                // ),
-                // const SizedBox(
-                //   height: 20,
-                // ),
                 //
                 //2nd Container
                 //
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -6,3 +6,5 @@ const mainbackground = Color(0xfffccdcd);
 const textColor = Color(0xff080826);
 const maintextColor = Colors.white;
 const lightblue = Color(0xffbae5f4);
+final mixColor =
+    Color.alphaBlend(const Color(0xffbae5f4), const Color(0xfffccdcd));
